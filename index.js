defda59b3e5ec0e7533575738c6ee3037b9b2ad0
gaganchittoria528462 <57328407+gaganchittoria528462@users.noreply.github.com>
2020-08-10 15:57:38 +0530

    COVID-MAP

---
+++ index.js
@@ -0,0 +1,29 @@
+function updatemap() {
+    fetch("https://www.trackcorona.live/api/provinces")
+        .then(response => response.json())
+        .then(rsp => {
+            console.log(rsp.data)
+            rsp.data.forEach(element => {
+                longitude = element.longitude;
+                latitude = element.latitude;
+
+                cases = element.confirmed;
+                if (cases > 255) {
+                    colors = "rgb(255,0,0)"
+                } else {
+                    colors = `rgb(${cases},0,0)`
+                }
+
+                // Maarker
+                new mapboxgl.Marker({
+                        draggable: false,
+                        color: colors,
+                    })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        })
+}
+updatemap();
+
+//https://api.covid19api.com/summary
